a7c25dad812b6d618e57be41028b899474bec82d
laasyasree-glitch <laasyasree.20bcd7033@vitap.ac.in>
2022-07-06 19:41:19 +0530

    token update

---
+++ src/main/java/com/dailycodebuffer/JWTDEMO/service/UserService.java
@@ -0,0 +1,20 @@
+package com.dailycodebuffer.JWTDEMO.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype. Service;
+@Service
+public class UserService implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// Logic to get the user from database
+		
+		return new User("admin","password",new ArrayList<>());
+	}
+	
+}
